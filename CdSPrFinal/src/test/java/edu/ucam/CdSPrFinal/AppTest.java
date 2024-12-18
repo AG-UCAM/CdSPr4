2d7d76066023f5f5af8802da9f3ad505407021f7
alexg <alexg@DESKTOP-45AG5F8>
2024-12-18 19:32:18 +0100

    Second commit

--- CdSPrFinal/src/test/java/edu/ucam/CdSPrFinal/AppTest.java
+++ CdSPrFinal/src/test/java/edu/ucam/CdSPrFinal/AppTest.java
@@ -7,32 +7,47 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    private Campeon c1;
+    private Campeon c2;
+
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        // Initialize Campeon objects before each test
+        String nombre = null;
+        c1 = new Campeon("c1", "t1", 3000, 400, 200);
+		c2 = new Campeon("c2", "t2", 2500, 500, 250);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        // Cleanup if necessary
+        c1 = null;
+        c2 = null;
     }
 
     /**
-     * Rigourous Test :-)
+     * Test if Campeon objects are initialized correctly.
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testCampeonInitialization() {
+        assertNotNull("El nombre no puede estar vacio", c1.getNombre());
+        assertNotNull("La vida no puede estar vacía", c1.getPuntos_vida());
+
+        assertNotNull("El daño no puede estar vacío", c2.getDano_ataque());
+        assertNotNull("La defensa no debe estar vacía", c2.getDefensa());
     }
 }
